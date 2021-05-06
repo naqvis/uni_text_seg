434ab9afab9b79e11fb84ac2ab7b8c6bd0f4ac30
Ali Naqvi <syed.alinaqvi@gmail.com>
2021-05-07 02:34:33 +0800

    Removed Array literal with dynamic population to improve compilation speed

--- src/textseg.cr
+++ src/textseg.cr
@@ -1,7 +1,7 @@
 # Shard `TextSegment` implements Unicode Text Segmentation according to [Unicode Standard Annex #29](http://unicode.org/reports/tr29/) (Unicode version 12.0.0)
 # to determine the grapheme cluster boundaries of unicode text.
 module TextSegment
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 
   # Returns a grapheme cluster iterator
   def self.graphemes(str : String)
